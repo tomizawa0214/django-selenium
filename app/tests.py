e25ab3efcf2133f3a836a4473e1423862bb3b1cf
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-12-19 19:24:00 +0900

    selenium

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from django.urls import reverse_lazy
+from selenium.webdriver.chrome.webdriver import WebDriver
 
-# Create your tests here.
+
+class LoginTest(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver(executable_path='./app/driver/chromedriver.exe')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        # ログインページを開く
+        self.selenium.get('https://paiza.jp/sign_in' + str(reverse_lazy('login')))
+
+        # ログイン
+        username_input = self.selenium.find_element_by_name('username')
+        username_input.send_keys('username')
+        password_input = self.selenium.find_element_by_name('password')
+        password_input.send_keys('password')
+        self.selenium.find_element_by_class_name('btn').click()
+
+        # ページタイトルの検証
+        self.assertEquals('ログイン後ページ', self.selenium.title)
+
+# import time                            # スリープを使うために必要
+# from selenium import webdriver         # Webブラウザを自動操作する（python -m pip install selenium)
+# import chromedriver_binary             # パスを通すためのコード
+
+# driver = webdriver.Chrome()            # Chromeを準備
+# driver.get('https://www.google.com/')  # Googleを開く
+# time.sleep(5)                          # 5秒間待機
+# driver.quit() 
--- app/views.py
+++ app/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
 from django.views.generic import View
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'app/index.html')
+        # Seleniumをあらゆる環境で起動させるChromeオプション
+        options = Options()
+        options.add_argument('--disable-gpu');
+        options.add_argument('--disable-extensions');
+        options.add_argument('--proxy-server="direct://"');
+        options.add_argument('--proxy-bypass-list=*');
+        options.add_argument('--start-maximized');
+        options.add_argument('--headless'); # ヘッドレスモード
+
+        DRIVER_PATH = './app/driver/chromedriver.exe' 
+
+        # ブラウザの起動
+        driver = webdriver.Chrome(executable_path=DRIVER_PATH, chrome_options=options)
+
+        # Webページにアクセスする
+        url = '{{ https://www.yahoo.co.jp }}'
+        driver.get(url)
+
+        return driver
+        # return render(request, 'app/index.html')
